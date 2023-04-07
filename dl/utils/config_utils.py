9889dae1c7ebe64b6f4107a2aa65cbce9fb128c3
Zhao <x.zhao.y@gmail.com>
2023-04-06 23:08:48 -0700

    Fix bug in gin_get when default evals to False.

--- dl/utils/config_utils.py
+++ dl/utils/config_utils.py
@@ -4,5 +4,5 @@ def gin_get(param, default=None):
   try:
     return gin.query_parameter(param)
   except ValueError as e:
-    if default: return default
+    if default is not None: return default
     raise
