011494da222ffb219c94aa01363fd1f02c8f4938
Zhao <x.zhao.y@gmail.com>
2023-09-10 00:52:02 -0700

    Allow sampling aligned to start of each example, or random span.

--- dl/data/dataset.py
+++ dl/data/dataset.py
@@ -4,6 +4,7 @@ import os
 
 import gin
 import numpy as np
+import pandas as pd
 import torch
 
 # Important for this to be on SSD so it's fast for memmap.
@@ -12,7 +13,8 @@ DATA_ROOT = '/home/z/data/zetaqubit/dl/data'
 @gin.configurable
 class MemoryMappedDataset(torch.utils.data.Dataset):
   def __init__(self, name, split, block_size, dbg_num_blocks=None,
-               padding_token=None, min_nonpadding_tokens=64):
+               padding_token=None, min_nonpadding_tokens=64,
+               align_to_seq_start=False):
     path = f'{DATA_ROOT}/{name}/{split}.bin'
     self.data = np.memmap(path, dtype=np.uint16, mode='r')
     self.block_size = block_size
@@ -22,6 +24,13 @@ class MemoryMappedDataset(torch.utils.data.Dataset):
     if dbg_num_blocks is not None:
       self.dataset_size = min(self.dataset_size,
                               int(dbg_num_blocks * self.block_size) + 1)
+    if align_to_seq_start:
+      offsets = list(pd.read_csv(f'{path}.meta')['example_offset'])
+      offsets = list(filter(lambda x: x <= self.dataset_size - self.block_size,
+                            offsets))
+      self.seq_offsets = offsets
+    else:
+      self.seq_offsets = None
 
   def __len__(self):
     return self.dataset_size
@@ -29,7 +38,11 @@ class MemoryMappedDataset(torch.utils.data.Dataset):
   def __getitem__(self, _):
     # Break contract and return a random span.
     def random_span():
-      ix = torch.randint(self.dataset_size - self.block_size, (1,))
+      if self.seq_offsets:
+        ix = torch.randint(len(self.seq_offsets), (1,))
+        ix = self.seq_offsets[ix]
+      else:
+        ix = torch.randint(self.dataset_size - self.block_size, (1,))
       x = self.data[ix:ix+self.block_size]
       return x
     x = random_span()
